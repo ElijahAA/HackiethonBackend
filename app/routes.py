0bc1ad0af1bc45a575dcf595b00599898c7f327b
ElijahAA <oeeagu@gmail.com>
2021-04-03 20:53:58 +1100

    Attempt to fix CodeWithMe glitch

--- app/routes.py
+++ app/routes.py
@@ -10,11 +10,6 @@ from app.models import User, Todo
 def index():
     return render_template('index.html', current_page="home")
 
-@app.route('/')
-@app.route('/index')
-def index():
-    return render_template('index.html')
-
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -32,7 +27,7 @@ def login():
         return render_template('login.html', current_page="login", error='Invalid username or password')
 
     login_user(user)
-    return redirect(url_for('todo'))
+    return redirect(url_for('index'))
 
 
 @app.route('/logout')
@@ -43,12 +38,48 @@ def logout():
 
 @app.route('/sign-up', methods=['GET', 'POST'])
 def signup():
-    render_template('signUp.html')
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+
+    if request.method == 'GET':
+        return render_template('signUp.html', current_page="signup")
+
+    username = request.form['username']
+    first_name = request.form['first_name']
+    last_name = request.form['last_name']
+    email = request.form['email']
+    password = request.form['password']
+
+    if username == '' or first_name == '' or last_name == '' or email == '' or password == '':
+        return render_template('signUp.html', current_page="signup", error='Missing required fields')
+
+    existing_user = User.query.filter_by(username=username).first()
+    if existing_user is not None:
+        return render_template('signUp.html', current_page="signup", error="Username is already in use")
+
+    existing_user = User.query.filter_by(email=email).first()
+    if existing_user is not None:
+        return render_template('signUp.html', current_page="signup", error="Email is already in use")
+
+    user = User(username=username, first_name=first_name, last_name=last_name, email=email)
+    user.set_password(password)
+    db.session.add(user)
+    db.session.commit()
+    login_user(user)
+    return redirect(url_for('index'))
 
 
 @app.route('/todo')
 def todo():
-    pass
+    if request.method == 'GET':
+        return render_template('todo.html', current_page="todos", todos=Todo.query.all(), )
+    title = request.form['title']
+    description = request.form['description']
+    user = User.query.get(current_user.id)
+    newTodo = Todo(title=title, description=description, user=user)
+    db.session.add(newTodo)
+    db.session.commit()
+    return redirect('/todo')
 
 
 @app.route('/follow/<username>', methods=['POST'])
@@ -59,3 +90,4 @@ def follow(username):
 @app.route('/unfollow/<username>', methods=['POST'])
 def unfollow(username):
     pass
+
