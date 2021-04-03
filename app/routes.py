d28e21d6a95537bb47360ce55704f6f7fbf68d46
ElijahAA <oeeagu@gmail.com>
2021-04-03 18:30:47 +1100

    Added updating 'active' html page

--- app/routes.py
+++ app/routes.py
@@ -8,23 +8,28 @@ from app.models import User, Todo
 @app.route('/')
 @app.route('/index')
 def index():
+    return render_template('index.html', current_page="home")
+
+@app.route('/')
+@app.route('/index')
+def index():
     return render_template('index.html')
 
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'GET':
-        return render_template('login.html')
+        return render_template('login.html', current_page="login")
 
     username = request.form['username']
     password = request.form['password']
 
     if username == '' or password == '':
-        return render_template('login.html', error='Please enter both a username and a password')
+        return render_template('login.html', current_page="login", error='Please enter both a username and a password')
 
     user = User.query.filter_by(username=username).first()
     if user is None or not user.check_password(password):
-        return render_template('login.html', error='Invalid username or password')
+        return render_template('login.html', current_page="login", error='Invalid username or password')
 
     login_user(user)
     return redirect(url_for('todo'))
